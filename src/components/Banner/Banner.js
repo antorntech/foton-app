d4ddaf3ece997cb1625f93ae0873e426f84a9c5a
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-15 11:41:05 +0600

    add react slick slider

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Banner;

f2f0e41af77ece739221f274ec0266979d0f9c4e
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-15 12:08:04 +0600

    add button on banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import "./Banner.css";
 
 const Banner = () => {
   const settings = {
@@ -12,10 +13,32 @@ const Banner = () => {
     slidesToScroll: 1,
   };
   return (
-    <div>
+    <div className="py-5">
       <Slider {...settings}>
         <div>
-          <h3>1</h3>
+          <div className="wrapper">
+            <h1 className="text-5xl text-primary">
+              Just What Your Business Needs
+            </h1>
+            <p className="text-2xl p-5 lg:px-72 text-gray-400">
+              Make your awesome business idea a reality with Foton, the fresh
+              new theme from Mikado - custom made for modern startups
+            </p>
+            <div className="banner-button p-5">
+              <button
+                id="readMoreBtn"
+                className="btn btn-secondary mx-2 px-10 rounded-full"
+              >
+                READ MORE
+              </button>
+              <button
+                id="contactUsBtn"
+                className="btn bg-blue-500 mx-2 px-10 rounded-full"
+              >
+                CONTACT US
+              </button>
+            </div>
+          </div>
         </div>
         <div>
           <h3>2</h3>
